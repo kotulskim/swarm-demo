a5448e9ba763b76b0380bd6951c33cf003c18d7d
kotulsm <marcin.kotulski@play.pl>
2026-04-28 15:26:09 -0400

    initial swarm demo

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"Hello from Docker Swarm! Host: {socket.gethostname()}"
+
+app.run(host="0.0.0.0", port=5005)
