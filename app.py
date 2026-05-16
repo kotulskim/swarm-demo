458afd4656a3ade1031f1304c321c370d3a1f6ce
kotulsm <marcin.kotulski@play.pl>
2026-05-16 10:04:24 -0400

    add environment configuration

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 import socket
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello from Docker Swarm v2! Host: {socket.gethostname()}"
+    app_env = os.getenv("APP_ENV", "DEV")
+    app_version = os.getenv("APP_VERSION", "unknown")
+
+    return f"""
+    <h1>Hello from Docker Swarm!</h1>
+    <p><b>Environment:</b> {app_env}</p>
+    <p><b>Version:</b> {app_version}</p>
+    <p><b>Host:</b> {socket.gethostname()}</p>
+    """
 
 @app.route("/health")
 def health():
