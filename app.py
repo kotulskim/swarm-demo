3e880efd461a4b4f8e5067dece6654dfba189661
kotulsm <marcin.kotulski@play.pl>
2026-05-16 09:36:56 -0400

    tags

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello from Docker Swarm v2! Host: {socket.gethostname()}"
+    return f"Hello from Docker Swarm v3! Host: {socket.gethostname()}"
 
 app.run(host="0.0.0.0", port=5005)
